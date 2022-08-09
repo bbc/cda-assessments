0fcf11c4fa8a93f7a88b6a989dd16edad00fcba6
Freddie Eglese <97610302+FEgleseBBC@users.noreply.github.com>
2022-08-09 17:13:18 +0100

    test: country details test

---
+++ commonwealth-front-end-task/src/components/CountryDetails/CountryDetails.test.js
@@ -0,0 +1,24 @@
+import { render, screen } from '@testing-library/react'
+
+import CountryDetails from './CountryDetails'
+
+describe('CountryDetails', () => {
+  it('should display all given details about the country', () => {
+    // Arrange
+    const country = {
+      name: 'Scotland',
+      id: 1,
+      code: 'SCO',
+      flagUrl: 'sample-website.com/image/scotland.png',
+    }
+
+    // Act
+    render(<CountryDetails details={country} />)
+
+    // Assert
+    expect(screen.getByText('Country: Scotland')).toBeInTheDocument()
+    expect(screen.getByText('Id: 1')).toBeInTheDocument()
+    expect(screen.getByText('Code: SCO')).toBeInTheDocument()
+    expect(screen.getByRole('img')).toHaveAttribute('src', 'sample-website.com/image/scotland.png')
+  })
+})
