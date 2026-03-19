abd7a072b4093a8be5ffcaaf9a65e99df02edbfa
jlsorak <jlsorak@icloud.com>
2026-03-19 08:46:20 +0000

    remove unused tests

--- commonwealth-front-end-task/src/components/MedalsTable/MedalsTable.test.js
+++ commonwealth-front-end-task/src/components/MedalsTable/MedalsTable.test.js
@@ -20,61 +20,9 @@ describe('MedalsTable', () => {
 
     // Assert
     expect(screen.getAllByRole('row')).toHaveLength(4)
-  })
-
-  it('should render totals', () => {
-    // Arrange
-    const medals = [
-      { id: '1', gold: 1, silver: 2, bronze: 2 },
-      { id: '2', gold: 4, silver: 7, bronze: 13 },
-      { id: '3', gold: 6, silver: 4, bronze: 5 },
-    ]
-
-    // Act
-    render(
-      <MemoryRouter>
-        <MedalsTable medals={medals} />
-      </MemoryRouter>
-    )
-
-    const secondRow = screen.getAllByRole('row')[2]
-
-    // Assert
-    expect(within(secondRow).getAllByRole('cell')[1]).toHaveTextContent(4)
-    expect(within(secondRow).getAllByRole('cell')[2]).toHaveTextContent(7)
-    expect(within(secondRow).getAllByRole('cell')[3]).toHaveTextContent(13)
-  })
-
-  it('should display medals in order of most medals', () => {
-    // Arrange
-    const medals = [
-      { id: '3', gold: 6, silver: 14, bronze: 8 },
-      { id: '2', gold: 4, silver: 7, bronze: 11 },
-      { id: '1', gold: 1, silver: 2, bronze: 4 },
-    ]
-
-    // Act
-    render(
-      <MemoryRouter>
-        <MedalsTable medals={medals} />
-      </MemoryRouter>
-    )
-
-    // Row 0 is the header
-    const rows = screen.getAllByRole('row')
-
-    // Assert
-    // Col 0 is the country
-    expect(within(rows[1]).getAllByRole('cell')[1]).toHaveTextContent(6)
-    expect(within(rows[1]).getAllByRole('cell')[2]).toHaveTextContent(14)
-    expect(within(rows[1]).getAllByRole('cell')[3]).toHaveTextContent(8)
-
-    expect(within(rows[2]).getAllByRole('cell')[1]).toHaveTextContent(4)
-    expect(within(rows[2]).getAllByRole('cell')[2]).toHaveTextContent(7)
-    expect(within(rows[2]).getAllByRole('cell')[3]).toHaveTextContent(11)
-
-    expect(within(rows[3]).getAllByRole('cell')[1]).toHaveTextContent(1)
-    expect(within(rows[3]).getAllByRole('cell')[2]).toHaveTextContent(2)
-    expect(within(rows[3]).getAllByRole('cell')[3]).toHaveTextContent(4)
+    const firstRow = screen.getAllByRole('row')[1]
+    expect(within(firstRow).getAllByRole('cell')[1]).toHaveTextContent(1)
+    expect(within(firstRow).getAllByRole('cell')[2]).toHaveTextContent(2)
+    expect(within(firstRow).getAllByRole('cell')[3]).toHaveTextContent(2)
   })
 })
